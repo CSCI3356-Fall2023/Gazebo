1e3950b3abd20ee5bf63c2db897fefb5ffcbc31c
nikkilock <lockwoni@bc.edu>
2023-10-19 13:36:11 -0400

    Started state capabilities

--- gazebo/gazebo/models.py
+++ gazebo/gazebo/models.py
@@ -28,3 +28,7 @@ class Student(models.Model):
     graduation_year = models.CharField(max_length=10)
 
 # Other models should probably be Watch, SystemState, etc.
+
+class SystemState(models.Model):
+    state = models.CharField(max_Length=10)
+    semester = models.CharField(max_length=15)
