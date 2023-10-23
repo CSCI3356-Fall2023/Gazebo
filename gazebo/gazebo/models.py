d2118777c324edf62197ca7715a2d24aa5789be4
AspiringShoulder <capstonebls@gmail.com>
2023-10-22 23:07:34 -0400

    Update models.py

--- gazebo/gazebo/models.py
+++ gazebo/gazebo/models.py
@@ -17,13 +17,13 @@ class Course(models.Model):
     section = models.CharField(max_length=10)
     instructor = models.CharField(max_length=255)
     # look into making days field a list
-    days = models.CharField()
+    days = models.CharField(max_length=10)
     start_time = models.TimeField()
     end_time = models.TimeField()
     location = models.CharField(max_length=255)
-    capacity = models.IntegerField()
+    capacity = models.IntegerField(default=0)
     current_enrollment = models.IntegerField(default=0)
-    num_watches = models.IntegerField()
+    num_watches = models.IntegerField(default=0)
     is_open = models.BooleanField()
     
 class Student(models.Model):
@@ -35,14 +35,14 @@ class Student(models.Model):
 
 
 class Watch(models.Model):
-    student_id = models.CharField()
-    course_id = models.CharField()
-    num_students = models.IntegerField()
+    student_id = models.CharField(max_length=10)
+    course_id = models.CharField(max_length=10)
+    num_students = models.IntegerField(default=0)
 
 class Log(models.model):
-    student_id = models.CharField()
+    student_id = models.CharField(max_length=10)
     timestamp = models.DateTimeField()
-    action = models.CharField()
+    action = models.CharField(max_length=10)
 
 class SystemState(models.Model):
     state = models.CharField(max_length=10)
