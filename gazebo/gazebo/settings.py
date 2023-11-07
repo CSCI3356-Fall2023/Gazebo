ad326509ff2f3695a85e94d0e0f527b37d8d953a
AspiringShoulder <77986642+AspiringShoulder@users.noreply.github.com>
2023-11-07 12:33:34 -0500

    Update settings.py to remove multiselectfield so we can run the server again

--- gazebo/gazebo/settings.py
+++ gazebo/gazebo/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gazebo',
-    'django_bootstrap5',
-    'MultiSelectField
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
